2ba49d132224bcc1101688568a6617ca40c23ae6
Mika Hänninen <mika.hanninen@gmail.com>
2012-11-23 16:45:33 +0200

    setup.py changed

--- setup.py
+++ setup.py
@@ -5,12 +5,10 @@ setup(
     version='0.1.0',
     author='Mika Hänninen',
     author_email='mika.hanninen@gmail.com',
-    packages=['generator'],
-    scripts=['generator/rfgen.py'],
+    scripts=['rfgen.py'],
     download_url='https://github.com/robotframework/Generator/tarball/master',
     url='http://github.com/robotframework/Generator/',
     license='LICENSE.txt',
     description='Script which generates a test project containing test libraries, test suites and resources.',
     long_description=open('README.md').read(),
-    install_requires=[],
 )
