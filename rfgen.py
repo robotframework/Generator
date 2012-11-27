738621db9f679ebb1549a516a1396f618d5ee74c
Mika Hänninen <mika.hanninen@gmail.com>
2012-11-27 13:09:36 +0200

    Refactoring

--- rfgen.py
+++ rfgen.py
@@ -31,62 +31,6 @@ src = os.path.join(ROOT, '..', 'src')
 sys.path.insert(0, lib)
 sys.path.insert(0, src)
 
-start_time = None
-end_time = None
-
-db_connection = None
-db_cursor = None
-
-common_tags = ['general','feature','important','regression','performance','usability']
-verbs = ['do','make','execute','select','count','process','insert','validate','verify','filter','magnify']
-words = ['abstraction','acetifier','acrodont','adenographical','advisableness','afterbreast','agrogeology',
-         'albuminoscope','alkarsin','Alsophila','American','amphitheatral','anapnoic','angiography','annulation',
-         'Anthoxanthum','antihelminthic','antisymmetrical','apoatropine','approximation','archgovernor','Arimaspian',
-         'arthrospore','asportation','atangle','audiometric','autolaryngoscopy','awlwort','backspierer','ballast',
-         'bargoose','Bathonian','becrawl','belatedness','bepaper','Bettina','bija','biriba','blastopore','blunge',
-         'bony','Bourbon','brandreth','Britain','Bryum','bur','cabbagewood','calciocarnotite','Campodea','capitulum',
-         'careener','cask','catstitch','censual','certify','chantey','chelydroid','chiromantical','chopa','Chrysotis',
-         'circumgyrate','clausure','clot','cocculiferous','cogue','coloring','companator','concordancer','conjointment',
-         'contemporary','coolness','cornigerous','costopleural','counterscrutiny','craniological','criniculture',
-         'cryoscopy','cunye','cyclamin','cyton','dapple','debtorship','deedbox','delignification','denitrator',
-         'dermatopnagic','detinet','dialogue','Dielytra','dioecious','discarnate','dishrag','dissertationist',
-         'dochmiac','dopebook','drainable','dubitant','dynamometer','ecostate','Eimak','elephantoidal','Emilia',
-         'enderonic','Enki','entreat','epimerite','equoidean','esophagoplasty','eudiometric','Evodia','existently',
-         'extensional','facsimilist','fasciculus','feminacy','fickly','firepower','flawful','flunkeyize','forbiddable',
-         'fork','frangula','frontomaxillary','furnishing','gallows','Gasterosteidae','Gemmingia','germanious',
-         'girdlingly','glossoptosis','Goldbird','gracelessly','greatheart','gruelly','gurl','Haemogregarinidae',
-         'handsomeness','hause','heedfulness','hemianopia','heptine','heterologous','highway','hoernesite',
-         'homoiothermic','horseway','humulene','hydromaniac','hypercorrection','hypogean','ichthyal','illaudation',
-         'impassioned','impuberal','incomprehension','indigitate','infare','iniquitously','insooth','intercombination',
-         'interpiece','intranatal','iodinophilic','irritomotile','isotomous','jaragua','jocundity','just','janne',
-         'Kedushshah','kiln','knowledging','labialization','lamboys','larkish','leadable','lenticularis','lexicologist',
-         'Limnoria','lithogenetic','logographical','loxotic','lycanthropist','macrotous','malacodermatous',
-         'manganeisen','Marianolatry','matador','mecometry','melastomaceous','merchantableness','Messiah','methylotic',
-         'microphotoscope','milsey','Mikko','miscompute','mistranslation','mollycoddling','monolocular','mora',
-         'mountainette','multimillion','Mussulwoman','myrmecology','Napoleonana','necrographer','nephrohydrosis',
-         'newsprint','noble','nonchalky','nonelemental','nonmalignant','nonresident','nonvolcanic','nubbling',
-         'obituarist','octary','oilcan','onca','opianyl','organizational','Ortol','Otomi','outroar','overcasting',
-         'overgrow','overregister','overwrought','pachyphyllous','paleface','pancyclopedic','papion','parareka',
-         'parsonese','patriarchalism','pedagogy','penetration','percolate','peripherically','perspiration',
-         'Phalangerinae','philocatholic','photoceramics','phyllomorphy','picrorhizin','pinguid','placer',
-         'platymesocephalic','plouky','poditti','polyaxial','polysporangium','porphyrin','postoperative','prankle',
-         'precoloration','predivinity','prelusion','presalvation','prevaricatory','probeer','progger','proportionality',
-         'Protoascales','prytanis','pseudospherical','publican','puppetize','pyrazolyl','quadruplex','quink',
-         'radiocarbon','Ranquel','reaggregation','recessively','recurve','reforestize','reinstruct','renderable',
-         'reprovably','respue','retrofracted','rhapsodie','rictus','robustly','rosoli','ruinator','sacciform','Salmon',
-         'Santos','sauqui','scatterbrain','scirtopodous','scrawler','sealant','selaginellaceous','semiglobe','senaite',
-         'serin','sextuplet','sheetwork','shortener','sighted','Singhalese','skelping','sleighty','Smithsonian',
-         'soapbush','somacule','souper','spectator','spicular','splinder','spumiform','stainproof','stearin',
-         'stethoscope','stoneworker','strich','stylite','subequality','subprincipal','succubine','sulphoterephthalic',
-         'supereminent','superstrong','survivalism','swollenly','synecdochism','tactite','tangence','Tashnakist',
-         'tectibranchiate','temporoalar','terral','Teutomania','theopneusty','thiofurane','thumblike','timbale',
-         'tobaccoism','topmast','toxicemia','transconductance','treading','trichromat','triphammer','trophodynamic',
-         'tubinarine','turnsheet','typographical','umbonule','unappeasedly','unbetray','unchangedness','unconditional',
-         'uncurst','underfeature','undertie','undowny','uneviscerated','unflaunted','ungovernable','unicameralist',
-         'unintersected','unlikelihood','unmollifiable','unpanel','unprecautioned','unreasonable','unride',
-         'unserviceable','unspicy','unswept','untroubledly','unwillingness','upthunder','urtication','vallancy',
-         'vegetation','vermiculite','victualer','visionist','voting','wany','Wazir','wheam','whuttering','wishedly',
-         'workbasket','xenium','yen','zeuglodont','zygophoric']
 
 def _create_test_libraries(path, filecount = 10, keywords=10):
     global db_cursor, verbs, words
@@ -99,16 +43,16 @@ def _create_test_libraries(path, filecount = 10, keywords=10):
         libs.append(lib_name)
         db_cursor.execute("INSERT INTO source (path,type) VALUES ('%s','CUSTOMLIBRARY')" % lib_name)
         libfile = open("%s/%s.py" % (path,lib_name),"w")
-        lib_doc = '"""Library documentation:\n' \
+        lib_doc = '"""Library documentation:\n'\
                   '\t%s"""' % lib_name
         libfile.write(\
-"""
-import os,time
+            """
+            import os,time
 
-class %s:
-    def __init__(self):
-        %s
-""" % (lib_name, lib_doc))
+            class %s:
+                def __init__(self):
+                    %s
+            """ % (lib_name, lib_doc))
 
         directory_looper = """for dirname, dirnames, filenames in os.walk('.'):
             for subdirname in dirnames:
@@ -131,16 +75,16 @@ class %s:
             db_cursor.execute("INSERT INTO keywords (name,source) VALUES ('%s','%s')" % (kw_name,lib_name))
             kw_doc = '"""Keyword documentation for %s"""' % kw_name
             libfile.write(\
-"""
-    def %s(self):
-        %s
-        %s
-""" % (kw_name,kw_doc,random.choice([directory_looper, sleeper, "pass"])))
+                """
+                    def %s(self):
+                        %s
+                        %s
+                """ % (kw_name,kw_doc,random.choice([directory_looper, sleeper, "pass"])))
 
         libfile.write(\
-"""
-myinstance = %s()
-""" % lib_name)
+            """
+            myinstance = %s()
+            """ % lib_name)
         libfile.close()
 
     #initfile_lines = open("%s/__init__.txt" % path).readlines()
@@ -155,17 +99,17 @@ myinstance = %s()
 #            break
 #        index += 1
 
-    #fo = open("%s/__init__.txt" % path, "w")
-    #for line in initfile_lines:
-    #    fo.write(line)
-    #fo.close()
+#fo = open("%s/__init__.txt" % path, "w")
+#for line in initfile_lines:
+#    fo.write(line)
+#fo.close()
 
 
 def _create_test_suite(path, filecount = 1, testcount = 20, avg_test_depth = 5, test_validity = 1):
     global db_cursor, verbs, words, common_tags
 
     available_resources = db_cursor.execute(
-                            "SELECT path FROM source WHERE type = 'RESOURCE' ORDER BY RANDOM()").fetchall()
+        "SELECT path FROM source WHERE type = 'RESOURCE' ORDER BY RANDOM()").fetchall()
     for testfile_index in range(filecount):
         libraries_in_use = {}
         resources_in_use = []
@@ -198,7 +142,7 @@ def _create_test_suite(path, filecount = 1, testcount = 20, avg_test_depth = 5, 
                         break
             tc_name = "Test %s in %s #%d" % (random.choice(verbs), selected_library.split("CustomLib")[1], tc)
             available_keywords = db_cursor.execute("SELECT * FROM keywords WHERE source IN ('%s','BuiltIn','OperatingSystem','String') ORDER BY RANDOM()"
-                                                    % selected_library).fetchall()
+                                                   % selected_library).fetchall()
             kwlib = random.choice([selected_library, testlib, testlib + "xyz"])
             test_txt += "%s\t[Documentation]\t%s" % (tc_name, "Test %d - %s\\n\\n%s" % (tc,strftime("%d.%m.%Y %H:%M:%S"),random.choice(words).strip()))
             test_tag = random.choice(common_tags)
@@ -288,7 +232,7 @@ def _create_test_resources(path, resources_in_file, resource_count, subdir = "")
         content += "\n*** Variables ***\n"
         for x in range(resource_count):
             content += "%-25s%10s%d\n" % ("${%s%d}" % (random.choice(words).strip().capitalize(),x),"",
-                                                        random.randint(1,1000))
+                                          random.randint(1,1000))
         content += "\n*** Keywords ***\n"
         resfile_ondisk.write(content)
         resfile_ondisk.close()
@@ -305,11 +249,12 @@ def _create_test_project(thetestdir,testlibs_count=5,keyword_count=10,testsuite_
     %d test steps per test case (option 'e')
     %d resource files (option 'f')
     %d resources per resource file (option 'r')""" % (testlibs_count, keyword_count, testsuite_count,
-                                        tests_in_suite, avg_test_depth, resource_count, resources_in_file)
+                                                      tests_in_suite, avg_test_depth, resource_count, resources_in_file)
 
     _create_test_libraries(thetestdir, filecount=testlibs_count, keywords=keyword_count)
     _create_test_resources(thetestdir,subdir="resources", resources_in_file=resource_count,resource_count=resources_in_file)
     _create_test_suite(thetestdir, filecount=testsuite_count, testcount=tests_in_suite, avg_test_depth=avg_test_depth,test_validity=test_validity)
+
 
 def main(path,testlibs_count=25,keyword_count=10,testsuite_count=30,tests_in_suite=40,resource_count=10,
          resources_in_file=100,avg_test_depth=3,test_validity=1):
@@ -346,6 +291,66 @@ def main(path,testlibs_count=25,keyword_count=10,testsuite_count=30,tests_in_sui
     result = "PASS"
     return result != 'FAIL'
 
+
+# Global variables
+start_time = None
+end_time = None
+
+db_connection = None
+db_cursor = None
+
+common_tags = ['general','feature','important','regression','performance','usability']
+verbs = ['do','make','execute','select','count','process','insert','validate','verify','filter','magnify']
+words = ['abstraction','acetifier','acrodont','adenographical','advisableness','afterbreast','agrogeology',
+         'albuminoscope','alkarsin','Alsophila','American','amphitheatral','anapnoic','angiography','annulation',
+         'Anthoxanthum','antihelminthic','antisymmetrical','apoatropine','approximation','archgovernor','Arimaspian',
+         'arthrospore','asportation','atangle','audiometric','autolaryngoscopy','awlwort','backspierer','ballast',
+         'bargoose','Bathonian','becrawl','belatedness','bepaper','Bettina','bija','biriba','blastopore','blunge',
+         'bony','Bourbon','brandreth','Britain','Bryum','bur','cabbagewood','calciocarnotite','Campodea','capitulum',
+         'careener','cask','catstitch','censual','certify','chantey','chelydroid','chiromantical','chopa','Chrysotis',
+         'circumgyrate','clausure','clot','cocculiferous','cogue','coloring','companator','concordancer','conjointment',
+         'contemporary','coolness','cornigerous','costopleural','counterscrutiny','craniological','criniculture',
+         'cryoscopy','cunye','cyclamin','cyton','dapple','debtorship','deedbox','delignification','denitrator',
+         'dermatopnagic','detinet','dialogue','Dielytra','dioecious','discarnate','dishrag','dissertationist',
+         'dochmiac','dopebook','drainable','dubitant','dynamometer','ecostate','Eimak','elephantoidal','Emilia',
+         'enderonic','Enki','entreat','epimerite','equoidean','esophagoplasty','eudiometric','Evodia','existently',
+         'extensional','facsimilist','fasciculus','feminacy','fickly','firepower','flawful','flunkeyize','forbiddable',
+         'fork','frangula','frontomaxillary','furnishing','gallows','Gasterosteidae','Gemmingia','germanious',
+         'girdlingly','glossoptosis','Goldbird','gracelessly','greatheart','gruelly','gurl','Haemogregarinidae',
+         'handsomeness','hause','heedfulness','hemianopia','heptine','heterologous','highway','hoernesite',
+         'homoiothermic','horseway','humulene','hydromaniac','hypercorrection','hypogean','ichthyal','illaudation',
+         'impassioned','impuberal','incomprehension','indigitate','infare','iniquitously','insooth','intercombination',
+         'interpiece','intranatal','iodinophilic','irritomotile','isotomous','jaragua','jocundity','just','janne',
+         'Kedushshah','kiln','knowledging','labialization','lamboys','larkish','leadable','lenticularis','lexicologist',
+         'Limnoria','lithogenetic','logographical','loxotic','lycanthropist','macrotous','malacodermatous',
+         'manganeisen','Marianolatry','matador','mecometry','melastomaceous','merchantableness','Messiah','methylotic',
+         'microphotoscope','milsey','Mikko','miscompute','mistranslation','mollycoddling','monolocular','mora',
+         'mountainette','multimillion','Mussulwoman','myrmecology','Napoleonana','necrographer','nephrohydrosis',
+         'newsprint','noble','nonchalky','nonelemental','nonmalignant','nonresident','nonvolcanic','nubbling',
+         'obituarist','octary','oilcan','onca','opianyl','organizational','Ortol','Otomi','outroar','overcasting',
+         'overgrow','overregister','overwrought','pachyphyllous','paleface','pancyclopedic','papion','parareka',
+         'parsonese','patriarchalism','pedagogy','penetration','percolate','peripherically','perspiration',
+         'Phalangerinae','philocatholic','photoceramics','phyllomorphy','picrorhizin','pinguid','placer',
+         'platymesocephalic','plouky','poditti','polyaxial','polysporangium','porphyrin','postoperative','prankle',
+         'precoloration','predivinity','prelusion','presalvation','prevaricatory','probeer','progger','proportionality',
+         'Protoascales','prytanis','pseudospherical','publican','puppetize','pyrazolyl','quadruplex','quink',
+         'radiocarbon','Ranquel','reaggregation','recessively','recurve','reforestize','reinstruct','renderable',
+         'reprovably','respue','retrofracted','rhapsodie','rictus','robustly','rosoli','ruinator','sacciform','Salmon',
+         'Santos','sauqui','scatterbrain','scirtopodous','scrawler','sealant','selaginellaceous','semiglobe','senaite',
+         'serin','sextuplet','sheetwork','shortener','sighted','Singhalese','skelping','sleighty','Smithsonian',
+         'soapbush','somacule','souper','spectator','spicular','splinder','spumiform','stainproof','stearin',
+         'stethoscope','stoneworker','strich','stylite','subequality','subprincipal','succubine','sulphoterephthalic',
+         'supereminent','superstrong','survivalism','swollenly','synecdochism','tactite','tangence','Tashnakist',
+         'tectibranchiate','temporoalar','terral','Teutomania','theopneusty','thiofurane','thumblike','timbale',
+         'tobaccoism','topmast','toxicemia','transconductance','treading','trichromat','triphammer','trophodynamic',
+         'tubinarine','turnsheet','typographical','umbonule','unappeasedly','unbetray','unchangedness','unconditional',
+         'uncurst','underfeature','undertie','undowny','uneviscerated','unflaunted','ungovernable','unicameralist',
+         'unintersected','unlikelihood','unmollifiable','unpanel','unprecautioned','unreasonable','unride',
+         'unserviceable','unspicy','unswept','untroubledly','unwillingness','upthunder','urtication','vallancy',
+         'vegetation','vermiculite','victualer','visionist','voting','wany','Wazir','wheam','whuttering','wishedly',
+         'workbasket','xenium','yen','zeuglodont','zygophoric']
+
+
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option("-l", "--libs", dest="libs",help="Number of test libraries", default=5)
@@ -355,9 +360,8 @@ if __name__ == '__main__':
     parser.add_option("-f", "--resourcefiles", dest="resourcefiles",help="Number of resource files", default=1)
     parser.add_option("-r", "--resources", dest="resources",help="Number of resources in a file", default=30)
     parser.add_option("-v", "--validity", dest="validity",help="Validity of test cases (1...0). To have ~80% passes give 0.8. Default 1.", default=1)
-    parser.add_option("-d", "--dir", dest="dir",help="Target directory for the test project", default="theproject")
     parser.add_option("-e", "--testdepth", dest="testdepth", help="Average number of steps in a test case (2..20)", default=3)
-
+    parser.add_option("-d", "--dir", dest="dir",help="Target directory for the test project", default="theproject")
     (options, args) = parser.parse_args()
 
     sys.path.insert(0, '.')
